e0b569ab79f8cc6ee19a0b315696d54c13af5c3e
Pavel-Reimer <pavel@example.com>
2025-09-06 11:44:56 +0000

    Fix STATIC_URL/STATIC_ROOT to tutorial values

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ststatic_backendatic/'
 STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
